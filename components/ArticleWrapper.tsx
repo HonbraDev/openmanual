dbb54c2f54d44e351e9647eaf23058ef12f63158
HonbraDev <54026315+HonbraDev@users.noreply.github.com>
2021-03-15 16:54:59 +0100

    Readme and homepage improvements

--- components/ArticleWrapper.tsx
+++ components/ArticleWrapper.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function ArticleWrapper({ children }: { children: any }) {
-  return <div className="prose article my-8 px-8 mx-auto max-w-4xl overflow-hidden">{children}</div>;
+  return (
+    <>
+      <div className="mt-6 pb-4 px-6 mx-auto max-w-4xl border-b border-gray-300">
+        <Link href="/">
+          <h1 className=" text-2xl font-light cursor-pointer">OpenManual</h1>
+        </Link>
+      </div>
+      <div className="prose article my-8 px-8 mx-auto max-w-4xl overflow-hidden">
+        {children}
+      </div>
+    </>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,29 @@
 import { useState } from "react";
-
-const articles = ["pořídit snímek obrazovky", "zadat úkol v Teams"];
+import Link from "next/link";
 
 export default function Home() {
   const [searchTerm, setSearchTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const onSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const onSearch = (event: React.ChangeEvent<HTMLInputElement>) =>
     setSearchTerm(event.target.value);
-    fetch(`/api/search?searchTerm=${encodeURI(event.target.value)}`)
-      .then((res) => res.json())
-      .then(setResults);
-  };
+
   return (
     <>
-      <div className="w-full bw-white p-8 h-screen">
-        {/* from-blue-400 to-blue-600 */}
-        <div className="max-w-xl mx-auto">
-          <h1 className="text-5xl font-bold text-shadow text-transparent bg-clip-text bg-gradient-to-br from-yellow-400 to-yellow-500">
-            Otevřený manuál pro online&nbsp;výuku
-          </h1>
-
-          <div className="text-center">
+      <div className="mt-6 pb-4 px-6 mx-auto max-w-4xl border-b border-gray-300">
+        <Link href="/">
+          <h1 className=" text-2xl font-light cursor-pointer">OpenManual</h1>
+        </Link>
+      </div>
+      <div className="my-6 px-6 mx-auto max-w-4xl">
+        <div className="prose max-w-full">
+          <h1>Otevřený manuál pro online výuku</h1>
+          <div className="text-center bg-gray-100 m-4 rounded-lg">
             <input
-              className="shadow-md rounded-lg px-6 py-3 mt-8 text-xl focus:shadow-lg transition-all max-w-sm focus:max-w-md focus:outline-none w-full"
+              className="shadow-md rounded-lg px-6 py-3 m-4 text-xl focus:shadow-lg transition-shadow max-w-md focus:outline-none w-full"
               placeholder="Hledat článek..."
               value={searchTerm}
               onInput={onSearch}
             />
           </div>
-
-          {searchTerm ? (
-            <div className="mt-8">
-              {results.map((article) => (
-                <div className="bg-white p-4 m-2 rounded-lg shadow-md cursor-pointer">
-                  Jak {article}
-                </div>
-              ))}
-            </div>
-          ) : (
-            <div />
-          )}
         </div>
       </div>
     </>
